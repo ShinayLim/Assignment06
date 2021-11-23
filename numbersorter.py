a6d11c489d9a309e770173a8eb9ab5ecf57fd661
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-23 15:14:37 +0800

    indention

--- numbersorter.py
+++ numbersorter.py
@@ -3,18 +3,18 @@ def inputValues():
     b = float(input("Enter b: "))
     c = float(input("Enter c: "))
     d = float(input("Enter d: "))
-
-if a < b:
+    
+    if a < b:
     a,b = b,a
-if a < c:
+    if a < c:
     a,c = c,a
-if a < d:
+    if a < d:
     a,d = d,a
-if b < c:
+    if b < c:
     b,c = c,b
-if b < d:
+    if b < d:
     b,d = d,b
-if c < d:
+    if c < d:
     c,d = d,c
     
 print (a, ">", b, ">", c, ">", d)
