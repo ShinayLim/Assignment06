16d9adf0d79798ed79b40bc946a552d1e01dc0c4
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-23 15:13:06 +0800

    base

---
+++ numbersorter.py
@@ -0,0 +1,19 @@
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+d = float(input("Enter d: "))
+
+if a < b:
+    a,b = b,a
+if a < c:
+    a,c = c,a
+if a < d:
+    a,d = d,a
+if b < c:
+    b,c = c,b
+if b < d:
+    b,d = d,b
+if c < d:
+    c,d = d,c
+    
+print (a, ">", b, ">", c, ">", d)
