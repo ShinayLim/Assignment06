3d6a815f1dbce3d63e24942c393cc04e7feeb7a2
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-24 20:57:05 +0800

    remove quiz night coz i find it corny lol

--- AdditionTrainer.py
+++ AdditionTrainer.py
@@ -5,10 +5,8 @@ score = int(score)
 
 name = input("What is your name? ")
 name = name.title()
-print("""Hello {}, welcome to Quiz night! 
-You will be presented with 10 questions.
-Enter the appropriate answer to each question
-Good luck!""".format(name))
+print("""Hello {}, You will be presented with 10 questions.
+Enter the appropriate answer to each question. Good luck!""".format(name))
 print("\n")
 
 #item1
