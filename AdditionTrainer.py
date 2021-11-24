4720ee332275b57250ca3a041eddab1b127ba6f1
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-24 20:48:02 +0800

    players name

---
+++ AdditionTrainer.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+name = name.title()
+print("""Hello {}, welcome to Quiz night! 
+You will be presented with 10 questions.
+Enter the appropriate answer to each question
+Good luck!""".format(name))
+print("\n")
