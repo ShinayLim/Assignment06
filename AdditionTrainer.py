943e96e86bfbf5c9e34577b7af05d5ff597fa8c0
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-24 20:49:33 +0800

    item1

--- AdditionTrainer.py
+++ AdditionTrainer.py
@@ -2,10 +2,25 @@ import random
 score = 0
 score = int(score)
 
-name = input("What is your name? ")
+
+name = input("What is your name?")
 name = name.title()
 print("""Hello {}, welcome to Quiz night! 
 You will be presented with 10 questions.
 Enter the appropriate answer to each question
 Good luck!""".format(name))
 print("\n")
+
+#item1
+addend1 = random.randint(0,99)
+addend2 = random.randint(0,99)
+print(str(addend1), "+", str(addend2), "= ?")
+answer1 = int(input("Your answer is:"))
+
+if(addend1 + addend2 == answer1):
+    print("Well done! that is correct!")
+    score = score + 1
+else:
+    print("Sorry, that is not correct")
+    
+print("Your current score is " + str(score) + " out of 10")
