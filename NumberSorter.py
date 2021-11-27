cbc2d50be7db3f877679870b6e389c0d8ca6584e
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-27 14:40:18 +0800

    replace letters with proper names

--- NumberSorter.py
+++ NumberSorter.py
@@ -1,24 +1,24 @@
 def inputValues():
-    a = float(input("Enter first value: "))
-    b = float(input("Enter second value: "))
-    c = float(input("Enter third value: "))
-    d = float(input("Enter fourth value: "))
+    value1= float(input("Enter first value: "))
+    value2 = float(input("Enter second value: "))
+    value3 = float(input("Enter third value: "))
+    value4 = float(input("Enter fourth value: "))
  
-    if a < b:
-        a,b = b,a
-    if a < c:
-        a,c = c,a
-    if a < d:
-        a,d = d,a
-    if b < c:
-        b,c = c,b
-    if b < d:
-        b,d = d,b
-    if c < d:
-        c,d = d,c
+    if value1 < value2:
+        value1,value2 = value2,value1
+    if value1 < value3:
+        value1,value3 = value3,value1
+    if value1 < value4:
+        value1,value4= value4,value1
+    if value2 < value3:
+        value2,value3 = value3,value2
+    if value2 < value4:
+        value2,value4 = value4,value2
+    if value3 < value4:
+        value3,value4 = value4,value3
     print("\n")
     print("Arrangement of values in \033[1m descending order \033[0m is:" )
-    print(a, ">", b, ">", c, ">", d)
+    print(value1, ">", value2, ">", value3, ">", value4)
     print("\n")
 
 inputValues()
